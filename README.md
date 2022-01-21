59703b106488296dd8f8469e4c50ff9382542049
Timothy Dowling <timothy.dowling@me.com>
2022-01-21 09:29:41 -0500

    Working

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OnSIS XML Modifier
+
 This program is designed to change errors in the XML Batch file,  It is designed to do this on a mass change level.
+
+This utility was designed to help cleanup the XML file produced by PowerSchool SIS
--- app.js
+++ app.js
@@ -1,36 +1,68 @@
+// OnSIS XML Cleaner
+// Created By: Tim Dowling
+
+// Required Files
 const xmljs = require('xml-js')
 const fs = require('fs')
 
-const filePath = './sample.xml'
+// Where the file is located and filename.
+const filePath = '/home/smooth/Work/OnSIS/batch/ONSIS_OCTELEM3_20211031_602620.xml'
+const fileLoc = '/home/smooth/Work/OnSIS/batch/'
 
+// Open the file
 fs.readFile(filePath, 'utf-8', (err, data)=> {
   if (err) return
+  
+  // Convert the XML file to JSON for easier access
   const xmlData = xmljs.xml2json(data, {compact: true,spaces: 2})
   const jsonData = JSON.parse(xmlData)
+
+  // Grab all the students
   const students = jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.SCHOOL.STUDENT
+
+  // Set the variables for re-creating the filename
   const report_year = jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.ACADEMIC_YEAR._text
   const report_period = jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.SUBMISSION_PERIOD_TYPE._text
   const school_number = jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.SCHOOL.SCHOOL_NUMBER._text
-  const fileName = report_year + "_" + report_period + "_" + school_number + ".xml"
+  const fileName = fileLoc + report_year + "_" + report_period + "_" + school_number + ".xml"
+
+  // Count how many records are being changed
   var counter = 0
+
+  // Loop through the students
   for (s in students){
+    
+    // Get all the keys in the Student enrollemnt sections
     for (key in students[s].STUDENT_SCHOOL_ENROLMENT){
+
+      // Find the Special Education section
       if (key == 'SPECIAL_EDUCATION'){
+
+        // If student has a certain type NONEXC then lets change it
         if (students[s].STUDENT_SCHOOL_ENROLMENT.SPECIAL_EDUCATION.EXCEPTIONALITY_TYPE._text == 'NONEXC'){
           jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.SCHOOL.STUDENT[s].STUDENT_SCHOOL_ENROLMENT.SPECIAL_EDUCATION.EXCEPTIONALITY_TYPE._text = ''
           jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.SCHOOL.STUDENT[s].STUDENT_SCHOOL_ENROLMENT.SPECIAL_EDUCATION.NON_IDENTIFIED_STUDENT_FLAG._text = 'T'
           jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.SCHOOL.STUDENT[s].STUDENT_SCHOOL_ENROLMENT.SPECIAL_EDUCATION.MAIN_EXCEPTIONALITY_FLAG._text = 'F'
           jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.SCHOOL.STUDENT[s].STUDENT_SCHOOL_ENROLMENT.SPECIAL_EDUCATION.IPRC_STUDENT_FLAG._text = 'F'
           jsonData.ONSIS_BATCH_FILE.DATA.SCHOOL_SUBMISSION.SCHOOL.STUDENT[s].STUDENT_SCHOOL_ENROLMENT.SPECIAL_EDUCATION.IPRC_REVIEW_DATE = {}
+          
+          // Update the counter
           counter += 1  
         }
       }
     }
   }
+
+  // Display the number of records that were changed
   console.log('Records Changed: ' + counter + '\nSAVING...')
+
+  // Convert the JSON to a string 
   jsonStr = JSON.stringify(jsonData)
+
+  // Convert the JSON back to XML
   const xmlData2 = xmljs.json2xml(jsonStr, {compact: true,spaces: 2})
 
+  // Create the file and save.
   fs.writeFile(fileName, xmlData2, 'utf-8', (err) => {
     console.log('Successful! ' + err)
   })
