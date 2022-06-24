d69e6199e726281eeaf32ca4542caf6d4e5f44b5
Tim Dowling <timothy.dowling@me.com>
2022-06-24 16:31:02 -0400

    Remove useless script

--- rea.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-file_loc = 'C:\\rea\\'
-file_name = '20212022_REA_Oct_Submission_CSV.csv' 
-file_path = file_loc + file_name
-
-data = pd.read_csv(file_path, index_col=0)
-
-print (data)
